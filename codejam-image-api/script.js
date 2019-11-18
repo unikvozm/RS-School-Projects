b175a94adc862d4043fccb1b61ed7e63511d1958
Natalia Volkova <natalia.volkofff@gmail.com>
2019-11-18 15:15:46 +0300

    refactor: apply eslint

--- codejam-image-api/script.js
+++ codejam-image-api/script.js
@@ -1,25 +1,24 @@
-const slider = document.querySelector(".drawing-area__slider");
-const value = document.querySelector(".drawing-area__slider-value");
+const slider = document.querySelector('.drawing-area__slider');
+const value = document.querySelector('.drawing-area__slider-value');
 
 let size;
 
-slider.onchange = function() {
+slider.onchange = () => {
   size = this.value;
-  let newPoint =
-    (this.value - this.getAttribute("min")) /
-    (this.getAttribute("max") - this.getAttribute("min"));
+  const newPoint = (this.value - this.getAttribute('min'))
+    / (this.getAttribute('max') - this.getAttribute('min'));
   let offset;
   switch (size) {
-    case "128":
+    case '128':
       offset = 7;
       break;
-    case "256":
+    case '256':
       offset = -7;
       break;
-    case "384":
+    case '384':
       offset = -21;
       break;
-    case "512":
+    case '512':
       offset = -35;
       break;
     default:
