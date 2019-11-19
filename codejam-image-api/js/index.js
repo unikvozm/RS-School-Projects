c08af5a4802d2e811a8d89c423bc8fe7e3e750e0
Natalia Volkova <natalia.volkofff@gmail.com>
2019-11-19 21:52:08 +0300

    feat: add colors-handlers

--- codejam-image-api/js/index.js
+++ codejam-image-api/js/index.js
@@ -7,6 +7,7 @@ const canvas = document.querySelector('.drawing-area__canvas');
 const ctx = canvas.getContext('2d');
 const previousColor = document.querySelector('.prev-color');
 const colorPicker = document.querySelector('.colors__color-picker');
+const colors = document.querySelectorAll('.colors__color');
 
 // Event listeners
 slider.onchange = () => setValueInRange();
@@ -27,3 +28,25 @@ window.onload = () => {
 window.onbeforeunload = () => {
   storage.setImage(canvas.toDataURL());
 };
+
+colorPicker.onchange = () => {
+  drawingArea.setCurrentColor(colorPicker.value);
+};
+
+colors.forEach((color, index) => {
+  color.addEventListener('click', () => {
+    switch (index) {
+      case 1:
+        drawingArea.setCurrentColor(drawingArea.prevColor);
+        break;
+      case 2:
+        drawingArea.setCurrentColor('#f74141');
+        break;
+      case 3:
+        drawingArea.setCurrentColor('#00BCD4');
+        break;
+      default:
+        break;
+    }
+  });
+});
